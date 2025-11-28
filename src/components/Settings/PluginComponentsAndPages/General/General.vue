a443aa38ef4d7f9bc672b1fda2b8ee91ccab8175
chs <jen.hs@outlook.com>
2025-11-29 01:57:04 +0800

    fix: 清理设置的旧代码

--- src/components/Settings/PluginComponentsAndPages/General/General.vue
+++ src/components/Settings/PluginComponentsAndPages/General/General.vue
@@ -31,6 +31,48 @@ const langOptions = computed(() => {
   ]
 })
 
+const openModeOptions = computed(() => {
+  return [
+    {
+      label: t('settings.link_opening_behavior_opt.current_tab'),
+      value: 'currentTab',
+    },
+    {
+      label: t('settings.link_opening_behavior_opt.current_tab_if_not_homepage'),
+      value: 'currentTabIfNotHomepage',
+    },
+    {
+      label: t('settings.link_opening_behavior_opt.background'),
+      value: 'background',
+    },
+    {
+      label: t('settings.link_opening_behavior_opt.new_tab'),
+      value: 'newTab',
+    },
+  ]
+})
+
+const videoCardOpenModeOptions = computed(() => {
+  return [
+    {
+      label: t('settings.link_opening_behavior_opt.current_tab'),
+      value: 'currentTab',
+    },
+    {
+      label: t('settings.link_opening_behavior_opt.drawer'),
+      value: 'drawer',
+    },
+    {
+      label: t('settings.link_opening_behavior_opt.background'),
+      value: 'background',
+    },
+    {
+      label: t('settings.link_opening_behavior_opt.new_tab'),
+      value: 'newTab',
+    },
+  ]
+})
+
 watch(() => settings.value.language, (newValue) => {
   locale.value = newValue
 })
@@ -59,6 +101,32 @@ watch(() => settings.value.language, (newValue) => {
           :options="langOptions"
           w="full"
         />
+      </SettingsItem>
+    </SettingsItemGroup>
+
+    <SettingsItemGroup :title="$t('settings.group_link_opening_behavior')">
+      <SettingsItem :title="$t('settings.top_bar_link_opening_behavior')">
+        <Select v-model="settings.topBarLinkOpenMode" :options="openModeOptions" w="full" />
+      </SettingsItem>
+      <SettingsItem :title="$t('settings.video_card_link_opening_behavior')">
+        <Select
+          v-model="settings.videoCardLinkOpenMode"
+          :options="videoCardOpenModeOptions"
+          w="full"
+        />
+      </SettingsItem>
+      <SettingsItem :title="$t('settings.search_bar_link_opening_behavior')">
+        <Select
+          v-model="settings.searchBarLinkOpenMode"
+          :options="openModeOptions"
+          w="full"
+        />
+      </SettingsItem>
+      <SettingsItem>
+        <template #title>
+          <div v-html="$t('settings.close_drawer_without_pressing_esc_again')" />
+        </template>
+        <Radio v-model="settings.closeDrawerWithoutPressingEscAgain" />
       </SettingsItem>
     </SettingsItemGroup>
 
