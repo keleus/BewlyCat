04cbdc920bdc1d867a0c7ff520110ba99715f122
chs <jen.hs@outlook.com>
2026-07-26 20:09:35 +0800

    refactor: 调整正在关注设置组

--- src/components/Settings/PluginComponentsAndPages/Home/Home.vue
+++ src/components/Settings/PluginComponentsAndPages/Home/Home.vue
@@ -458,36 +458,6 @@ function handleToggleHomeTab(tab: any) {
       </div>
     </SettingsItemGroup>
 
-    <SettingsItemGroup :title="$t('settings.group_following')">
-      <SettingsItem :title="$t('settings.use_following_new_layout')" :desc="$t('settings.use_following_new_layout_desc')" right-width="auto">
-        <Radio v-model="settings.useFollowingNewLayout" />
-      </SettingsItem>
-      <SettingsItem :title="$t('settings.enable_following_inactive_blacklist')" :desc="$t('settings.enable_following_inactive_blacklist_desc')" right-width="auto">
-        <Radio v-model="settings.enableFollowingInactiveBlacklist" />
-      </SettingsItem>
-      <template v-if="settings.enableFollowingInactiveBlacklist">
-        <SettingsItem :title="$t('settings.following_inactive_days')" :desc="$t('settings.following_inactive_days_desc')" right-width="auto">
-          <Input
-            v-model="settings.followingInactiveDays" type="number" :min="1" :max="365"
-            w-120px
-          >
-            <template #suffix>
-              <span text="sm $bew-text-2" whitespace-nowrap>{{ $t('common.days') }}</span>
-            </template>
-          </Input>
-        </SettingsItem>
-      </template>
-      <SettingsItem :title="$t('settings.following_tab_show_livestreaming_videos')" right-width="auto">
-        <Radio v-model="settings.followingTabShowLivestreamingVideos" />
-      </SettingsItem>
-      <SettingsItem :title="$t('settings.following_filter_charging_videos')" :desc="$t('settings.following_filter_charging_videos_desc')" right-width="auto">
-        <Radio v-model="settings.followingFilterChargingVideos" />
-      </SettingsItem>
-      <SettingsItem :title="$t('settings.following_filter_dynamic_videos')" :desc="$t('settings.following_filter_dynamic_videos_desc')" right-width="auto">
-        <Radio v-model="settings.followingFilterDynamicVideos" />
-      </SettingsItem>
-    </SettingsItemGroup>
-
     <SettingsItemGroup
       :title="$t('settings.group_home_tabs')"
     >
@@ -535,6 +505,41 @@ function handleToggleHomeTab(tab: any) {
       </SettingsItem>
       <SettingsItem :title="$t('settings.fixed_home_tabs_on_home_page')" right-width="auto">
         <Radio v-model="settings.fixedHomeTabsOnHomePage" />
+      </SettingsItem>
+    </SettingsItemGroup>
+
+    <SettingsItemGroup
+      :title="$t('settings.group_following')"
+      :desc="$t('settings.group_following_desc')"
+      collapsible
+      default-collapsed
+    >
+      <SettingsItem :title="$t('settings.use_following_new_layout')" :desc="$t('settings.use_following_new_layout_desc')" right-width="auto">
+        <Radio v-model="settings.useFollowingNewLayout" />
+      </SettingsItem>
+      <SettingsItem :title="$t('settings.enable_following_inactive_blacklist')" :desc="$t('settings.enable_following_inactive_blacklist_desc')" right-width="auto">
+        <Radio v-model="settings.enableFollowingInactiveBlacklist" />
+      </SettingsItem>
+      <template v-if="settings.enableFollowingInactiveBlacklist">
+        <SettingsItem :title="$t('settings.following_inactive_days')" :desc="$t('settings.following_inactive_days_desc')" right-width="auto">
+          <Input
+            v-model="settings.followingInactiveDays" type="number" :min="1" :max="365"
+            w-120px
+          >
+            <template #suffix>
+              <span text="sm $bew-text-2" whitespace-nowrap>{{ $t('common.days') }}</span>
+            </template>
+          </Input>
+        </SettingsItem>
+      </template>
+      <SettingsItem :title="$t('settings.following_tab_show_livestreaming_videos')" right-width="auto">
+        <Radio v-model="settings.followingTabShowLivestreamingVideos" />
+      </SettingsItem>
+      <SettingsItem :title="$t('settings.following_filter_charging_videos')" :desc="$t('settings.following_filter_charging_videos_desc')" right-width="auto">
+        <Radio v-model="settings.followingFilterChargingVideos" />
+      </SettingsItem>
+      <SettingsItem :title="$t('settings.following_filter_dynamic_videos')" :desc="$t('settings.following_filter_dynamic_videos_desc')" right-width="auto">
+        <Radio v-model="settings.followingFilterDynamicVideos" />
       </SettingsItem>
     </SettingsItemGroup>
 
